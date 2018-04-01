3cb2d9726a81ad6b7de816788de58b16cdd7795c
assassinbull <assassinbull@gmail.com>
2018-04-01 23:42:12 +0300

    Proje ve tag ler hakkında yardımcı metinler eklendi.

--- src/pages/item-detail/item-detail.ts
+++ src/pages/item-detail/item-detail.ts
@@ -1,7 +1,9 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
 
+import { CorpusTagModel } from '../../models/corpus-tag';
 import { Items } from '../../providers/providers';
+import { CorpusTag } from '../../providers/providers';
 
 @IonicPage()
 @Component({
@@ -11,8 +13,10 @@ import { Items } from '../../providers/providers';
 export class ItemDetailPage {
   item: any;
   activeProjectSubscriptions: any[];
+  corpusTagsModel: CorpusTagModel[];
 
-  constructor(public navCtrl: NavController, navParams: NavParams, public items: Items) {
+  constructor(public navCtrl: NavController, navParams: NavParams, public items: Items
+    , public corpusTag: CorpusTag) {
     this.item = navParams.get('item') || items.defaultItem;
   }
 
@@ -20,6 +24,11 @@ export class ItemDetailPage {
     this.items.getProjectSubscriptions({ projectId: this.item.Id }).subscribe((resp: any) => {
       this.activeProjectSubscriptions = resp.Data;
     });
+
+    this.corpusTag.get(this.item.Id)
+      .subscribe((resp: any) => {
+        this.corpusTagsModel = resp.Data;
+      });
   }
 
 }
